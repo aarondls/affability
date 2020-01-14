24c1a3ec349bd9ad6fa298dea610b9bb661a3c9b
Aaron de los Santos <hansaaron_d@yahoo.com>
2020-01-13 23:16:59 -0500

    Added speech recognition and text to speech

--- Brain.py
+++ Brain.py
@@ -1,1 +1,24 @@
-print("test")
+import speech_recognition as speechrecog
+import subprocess
+
+recog = speechrecog.Recognizer()
+mic = speechrecog.Microphone()
+
+print ("initialized speech recognition")
+
+def reply(response):
+    subprocess.call(['say', response])
+
+reply("voice response initialized")
+
+# ready
+
+with mic as source:
+    print("say something!")
+    recog.adjust_for_ambient_noise(source)
+    audio = recog.listen(source)
+    text = recog.recognize_google(audio)
+    #reply(text)
+    
+if "hello" in text:
+    reply("hello")
