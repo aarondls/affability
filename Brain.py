1cb7922facd9f9139505528a7be99fbaa2012301
Aaron de los Santos <hansaaron_d@yahoo.com>
2020-01-18 14:51:48 -0500

    Call action function based on detected intent

--- Brain.py
+++ Brain.py
@@ -15,12 +15,34 @@ def reply(response):
     text = recog.recognize_google(audio)
     reply("Wait a second") '''
 
+def alarm():
+    # detected intent is alarm and perform necessary tasks here
+    print("alarm function called")
+
+functions = {
+    "Alarm": alarm
+}
+
+keepGoing = True
+
 if __name__ == '__main__':
     print("ready")
-    text = input()
-    DialogflowAPITest.askBotResponse(text)
-    print(DialogflowAPITest.reply)
-    print(DialogflowAPITest.detectedIntent)
-    print(DialogflowAPITest.confidence)
-    print(DialogflowAPITest.requiredParamsPresent)
-    print(DialogflowAPITest.replyParams)
+    while keepGoing == True:
+        text = input()
+        if text == "End":
+            keepGoing = False
+            break
+        else:
+            DialogflowAPITest.askBotResponse(text)
+            print(DialogflowAPITest.reply)
+            print(DialogflowAPITest.detectedIntent)
+            print(DialogflowAPITest.confidence)
+            print(DialogflowAPITest.requiredParamsPresent)
+            if "time" in DialogflowAPITest.replyParams:
+                print(DialogflowAPITest.replyParams["time"])
+            # reply(DialogflowAPITest.reply)
+            if DialogflowAPITest.requiredParamsPresent == True:
+                #All parameters needed to process request is present
+                print("required parameters present")
+                if DialogflowAPITest.detectedIntent in functions:
+                    functions[DialogflowAPITest.detectedIntent]()
