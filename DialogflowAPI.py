ffd440295e7dfc7dfa65c4f59486b6cd28b6cc4c
Aaron de los Santos <hansaaron_d@yahoo.com>
2020-02-11 21:49:57 -0500

    Cleaned up DialogflowAPI file

--- DialogflowAPI.py
+++ DialogflowAPI.py
@@ -15,25 +15,12 @@ text_to_be_analyzed = "testing"
 session_client = dialogflow.SessionsClient()
 session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
 
-''' text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
-query_input = dialogflow.types.QueryInput(text=text_input)
-
-try:
-    response = session_client.detect_intent(session=session, query_input=query_input)
-except InvalidArgument:
-    raise
-'''
-
-# print("Query text:", response.query_result.query_text)
-# print("Detected intent:", response.query_result.intent.display_name)
-# print("Detected intent confidence:", response.query_result.intent_detection_confidence)
-# print("Fulfillment text:", response.query_result.fulfillment_text)
-
 def askBotResponse(text):
+    text_input = dialogflow.types.TextInput(text=text, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow.types.QueryInput(text=text_input)
     try:
-        text_input = dialogflow.types.TextInput(text=text, language_code=DIALOGFLOW_LANGUAGE_CODE)
-        query_input = dialogflow.types.QueryInput(text=text_input)
         response = session_client.detect_intent(session=session, query_input=query_input)
+        # Allow the variables to be called from outside
         global detectedIntent, confidence, reply, action, requiredParamsPresent, replyParams
         detectedIntent = response.query_result.intent.display_name
         confidence = response.query_result.intent_detection_confidence
@@ -42,13 +29,5 @@ def askBotResponse(text):
         requiredParamsPresent = response.query_result.all_required_params_present
         replyParams = MessageToDict(response.query_result.parameters)
     except InvalidArgument:
+        # raise exception
         return("Unable to process")
-
-# askBotResponse("hello")
-
-'''
-print(detectedIntent)
-print(confidence)
-print(reply)
-print(action)
-print(requiredParametersPresent) '''
