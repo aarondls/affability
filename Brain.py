164e023e5a6d10951ca0ccf9668dcb76095ce82c
Aaron de los Santos <hansaaron_d@yahoo.com>
2020-02-12 21:51:12 -0500

    Function can be called in a single line

--- Brain.py
+++ Brain.py
@@ -6,21 +6,13 @@ from dialogflow_v2.types import TextInput, QueryInput
 from google.api_core.exceptions import InvalidArgument
 from google.protobuf.json_format import MessageToDict
 
-# file path for DialogFlow json key
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '/Users/aarondelossantos/Documents/DialogueflowKey/SimpleassistantKey.json'
+# only for speech recognition
+''' recog = speechrecog.Recognizer()
+mic = speechrecog.Microphone() '''
 
-DIALOGFLOW_PROJECT_ID = 'simpleassistant-mwxwbe'
-DIALOGFLOW_LANGUAGE_CODE = 'en-US'
-SESSION_ID = 'me'
-
-session_client = dialogflow.SessionsClient()
-session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
-
-recog = speechrecog.Recognizer()
-mic = speechrecog.Microphone()
-
-def reply(response):
-    subprocess.call(['say', response])
+# to use speech to text response, call the following function
+''' def reply(response):
+    subprocess.call(['say', response]) '''
 
 # to use speech recognition, uncomment the following
 ''' with mic as source:
@@ -30,7 +22,7 @@ def reply(response):
     text = recog.recognize_google(audio)
     reply("Wait a second") '''
 
-#define functions
+# define functions, like this example alarm function
 def alarm():
     # detected intent is alarm and perform necessary tasks here
     print("alarm function called")
@@ -38,12 +30,32 @@ def alarm():
     if "time" in replyParams:
         print(replyParams["time"])
         
-#list all function names after defining it
+# list all function names after defining it
 functions = {
     "Alarm": alarm
 }
 
-def understand(text):
+class organizer:
+    def __init__(self, detectedIntent, confidence, reply, action, requiredParamsPresent, replyParams):
+        self.detectedIntent = detectedIntent
+        self.confidence = confidence
+        self.reply = reply
+        self.action = action
+        self.requiredParamsPresent = requiredParamsPresent
+        self.replyParams = replyParams
+
+
+def understand(text, credentials, projectID, languageCode, sessionID):
+    # file path for DialogFlow json key
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials
+
+    DIALOGFLOW_PROJECT_ID = projectID
+    DIALOGFLOW_LANGUAGE_CODE = languageCode
+    SESSION_ID = sessionID
+
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+
     text_input = dialogflow.types.TextInput(text=text, language_code=DIALOGFLOW_LANGUAGE_CODE)
     query_input = dialogflow.types.QueryInput(text=text_input)
     try:
@@ -59,16 +71,14 @@ def understand(text):
     except InvalidArgument:
         # raise exception
         return("Unable to process")
-    print(reply)
-    print(detectedIntent)
-    print(confidence)
-    print(requiredParamsPresent)
-    if requiredParamsPresent == True:
-        #All parameters needed to process request is present
-        print("required parameters present")
-        return detectedIntent
+    result = organizer(detectedIntent, confidence, reply, action, requiredParamsPresent, reply)
+    print(result.reply)
+    print(result.detectedIntent)
+    print(result.confidence)
+    print(result.requiredParamsPresent)
+    return result
 
-# Calls the function that matches detected intent
+# Calls the function that matches detected intent using the dictionary functions
 if __name__ == '__main__':
     print("ready")
     keepGoing = True
@@ -78,5 +88,9 @@ if __name__ == '__main__':
             keepGoing = False
             break
         else: 
-            if understand(text) in functions:
-                functions[detectedIntent]()
+            reply = understand(text, '/Users/aarondelossantos/Documents/DialogueflowKey/SimpleassistantKey.json', 'simpleassistant-mwxwbe', 'en-US', 'me') 
+            if requiredParamsPresent == True:
+                #All parameters needed to process request is present
+                print("required parameters present")
+                if reply.detectedIntent in functions:
+                    functions[detectedIntent]()
