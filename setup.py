4e783ab0e78e95a4866acf5089f25d9c44887076
Aaron de los Santos <hansaaron_d@yahoo.com>
2020-02-15 00:22:26 -0500

    Created setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setuptools.setup(
+    name="Affability",
+    version="0.0.1",
+    author="Aaron de los Santos",
+    author_email="hansaaron_d@yahoo.com",
+    description="A simple package for interfacing with Dialogflow",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aarondls/Affability",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
