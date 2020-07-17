32e9f5e46d40f630221349a3470b07a736c7e801
Aaron de los Santos <hansaaron_d@yahoo.com>
2020-07-16 22:37:00 -0700

    Added in-depth comments

--- Example_usage.py
+++ Example_usage.py
@@ -4,15 +4,15 @@ import subprocess
 # Import Affability
 import Affability
 
-# only for speech recognition
+# Only for speech recognition
 ''' recog = speechrecog.Recognizer()
 mic = speechrecog.Microphone() '''
 
-# to use speech to text response, call the following function
+# To use speech to text response, call the following function
 ''' def reply(response):
     subprocess.call(['say', response]) '''
 
-# to use speech recognition, uncomment the following
+# To use speech recognition, uncomment the following
 ''' with mic as source:
     print("say something!")
     recog.adjust_for_ambient_noise(source)
@@ -20,15 +20,17 @@ mic = speechrecog.Microphone() '''
     text = recog.recognize_google(audio)
     reply("Wait a second") '''
 
-# define functions, like this example alarm function
+# Define functions that can be called based on intent, like this example alarm function
 def alarm():
-    # detected intent is alarm and perform necessary tasks here
+    # Detected intent is alarm and perform necessary tasks here
     print("alarm function called")
-    # To find the parameters, first check if it exists then take it from the replyParam dictionary like so:
+    # To find the parameters, first verify that the parameter exists 
+    # then take it from the replyParam dictionary like so:
     if "time" in reply.replyParams:
         print(reply.replyParams["time"])
         
-# list all function names after defining it
+# List all function names after defining it, to allow it to be called from a string
+# For example, alarm() is called from the string "Alarm"
 functions = {
     "Alarm": alarm
 }
@@ -43,10 +45,13 @@ if __name__ == '__main__':
             keepGoing = False
             break
         else: 
+            # Change argument to filepath of Dialogueflow json key
             reply = Affability.understand(text, '/Users/aarondelossantos/Documents/DialogueflowKey/SimpleassistantKey.json', 'simpleassistant-mwxwbe', 'en-US', 'me') 
             print(reply.reply)
             if reply.requiredParamsPresent == True:
-                #All parameters needed to process request is present
+                # All parameters needed to process request is present
                 print("required parameters present")
+                # This is the purpose of listing function names above
                 if reply.detectedIntent in functions:
+                    # Call the necessary function based on the reply
                     functions[reply.detectedIntent]()
